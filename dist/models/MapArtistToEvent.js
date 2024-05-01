699bc7713b7059f47150302c45f157f421a8c305
Dustin Stokes <78513596+stokesdusty@users.noreply.github.com>
2024-04-30 19:04:45 -0700

    build update

---
+++ dist/models/MapArtistToEvent.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+const mapArtistToEventSchema = new mongoose_1.Schema({
+    artistName: {
+        type: String,
+        required: true,
+    },
+    eventId: {
+        type: Number,
+        required: true,
+    },
+});
+exports.default = (0, mongoose_1.model)("MapArtistToEvent", mapArtistToEventSchema);
+//# sourceMappingURL=MapArtistToEvent.js.map
